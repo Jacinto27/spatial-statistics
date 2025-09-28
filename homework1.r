ee7d7993a315a65b3aeba715e4b64c4db6399455
Jacinto <carlospenavalerio98@gmail.com>
2025-09-29 00:37:14 +0200

    On branch main
    Initial commit
    
    Changes to be committed:

---
+++ homework1.r
@@ -0,0 +1,4 @@
+# a) Build an R function to choose the parameter p in the inverse distance weighting estimator. Hint: use the idw implemented in gstat.
+# b) Work on the Wolfcamp dataset. Interpolate Wolfcamp data on a 20x20 grid (built from the coordinates) using IDW and multilevel bi-splines (function mba.surf from MBA) and compare the results using a proper score
+#  Hint: divide data into training and testing sets
+
