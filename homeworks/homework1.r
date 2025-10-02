42d837ce286714d47ca8879cebe4d235d91619af
Jacinto27 <90574028+Jacinto27@users.noreply.github.com>
2025-10-02 13:23:40 +0200

    Update homework1.r
    improved the comments

--- homeworks/homework1.r
+++ homeworks/homework1.r
@@ -1,62 +1,62 @@
 # a) Build an R function to choose the parameter p in the inverse distance weighting estimator. Hint: use the idw implemented in gstat.
 library(gstat)
 library(sf)
- 
+library(geoR) # wolfcamp data, also in MBA package
 
 data(wolfcamp)
 df <- wolfcamp
 #structure(df)
-# remove old yucky geodata structure and turn into a dataframe NOTE: MBA package has wolfcamp as dataframe, step not needed if using
+# remove old geodata structure and turn into a dataframe NOTE: MBA package has wolfcamp as dataframe, step not needed if using
 coords <- df$coords 
 vals <- df$data
 df2 <- data.frame(x = coords[, 1], y = coords[, 2], z = vals)
-#Define as sf
+#Define as sf with the nomenclature from class: x & y are coordinates, z are values
 pts <- st_as_sf(df2, coords = c("x", "y"), crs = NA)
 
 
-#leave one out for loop with rmse validation
-p_vals = c(1, 1.5, 2, 2.5, 3, 4, 5) #I assume theres greater value in miniscule differences between 1 and 3 than 4 and 5
+#leave-one-out loop with rmse validation
+p_vals = c(1, 1.5, 2, 2.5, 3, 4, 5) #I assume theres greater value in miniscule differences between 1 and 3 than 4 and 5, ask the prof.
 
-#score
+#error score
 rmse <- function(e) {
     sqrt(mean(e^2, na.rm = TRUE)) 
 }
 
 #function that loops
-loovcf <- function(pts, p_vals = c(1, 2, 3, 4), z_col = "z") { #maybe sane defalt for p? check with prof
+loovcf <- function(pts, p_vals = c(1, 2, 3, 4), z_col = "z") { #generic defaults defalt for p? check with prof
 
 z_vec <- st_drop_geometry(pts)$z
 scores <- numeric(length(p_vals))  # one RMSE per p
 
 
-for (j in seq_along(p_vals)) {
+for (j in seq_along(p_vals)) { # for each value in the list P
   p <- p_vals[j]
   cat("for p =", p, "...\n")
   errs <- numeric(nrow(pts))
 
-  for (i in seq_len(nrow(pts))) {
+  for (i in seq_len(nrow(pts))) {# run the regression for all values in the data except for 1, and do this until every single row has been removed and replaced 
     train <- pts[-i, ]     # all except i
     test  <- pts[i, ]    
     pred  <- idw(
-      formula   = z ~ 1,
+      formula   = z ~ 1, # is this formula correct?
       locations = train,
       newdata   = test,
       idp       = p
     )$var1.pred
-    errs[i] <- pred - z_vec[i]
+    errs[i] <- pred - z_vec[i] #store each error term into a list for each regression,
   }
 
-  scores[j] <- rmse(errs)
+  scores[j] <- rmse(errs) # calculate the mean square error of all regression for each p value
 }
 
-cv_table <- data.frame(p = p_vals, RMSE = scores)
+cv_table <- data.frame(p = p_vals, RMSE = scores) #store in table and 
 cv_table <- cv_table[order(cv_table$RMSE), ] # sort results by lowest error
 
 return(cv_table)
 } 
 
 results <- loovcf(pts, p_vals)
-## lowest rmse for p:4
+## lowest rmse for p:4 for wolfcamp data
 
 # b) Work on the Wolfcamp dataset. Interpolate Wolfcamp data on a 20x20 grid (built from the coordinates) using IDW and multilevel bi-splines (function mba.surf from MBA) and compare the results using a proper score
 #  Hint: divide data into training and testing sets
